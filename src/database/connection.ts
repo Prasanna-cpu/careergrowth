92516d0a1c3093fbe859667de0399ff6a8492960
Prasanna-cpu <kumar22maran@gmail.com>
2026-08-04 19:48:38 +0530

    commit database_connection

---
+++ src/database/connection.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+export const connectDB = async (uri : string) => {
+    try {
+        const connection = await mongoose.connect(uri);
+        console.info(`Connection successful : ${connection.connection.host}`)
+    }
+    catch (error)        {
+        console.error(`Error connecting to database: ${error}`)
+    }
+}
